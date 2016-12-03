d2223d66a9072da0e47ced5d7a84f95f1c3c559e
lillccian <lillccian@gmail.com>
2016-12-03 15:25:18 +0800

    before action set songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,19 +1,18 @@
 class SongsController < ApplicationController
 
+	before_action :set_song, :only=> [:show, :edit, :update]
+
 	def index
 		@songs = Song.all 
 	end
 	
 	def show 
-		@song = Song.find(params[:id])
 	end
 	
 	def edit 
-		@song = Song.find(params[:id])
 	end
 	
 	def update
-		@song = Song.find(params[:id])
 		if @song.update(song_params)
 			redirect_to songs_path
 		else
@@ -22,6 +21,10 @@ class SongsController < ApplicationController
 	end
 
 	private
+
+	def set_song
+		@song = Song.find(params[:id])
+	end
 
 	def song_params
 		params.require(:song).permit(:name, :link, :description)
