3851f5221fbb43237c03b8f745b9dfed6bfd8370
lillccian <lillccian@gmail.com>
2016-12-03 15:35:28 +0800

    g devise

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,8 @@
 class SongsController < ApplicationController
 
-	before_action :set_song, :only=> [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, :except => [:index]
+
+	before_action :set_song, :only => [:show, :edit, :update, :destroy]
 
 	def index
 		@songs = Song.all 
