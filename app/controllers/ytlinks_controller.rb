d7a862d6ebda3530b5bb897a3cb6482750a47447
lillccian <lillccian@gmail.com>
2016-12-03 22:20:54 +0800

    g ytlink edit

--- app/controllers/ytlinks_controller.rb
+++ app/controllers/ytlinks_controller.rb
@@ -1,19 +1,38 @@
 class YtlinksController < ApplicationController
 
 	before_action :set_song
+	before_action :set_ytlink, :only => [:show, :edit, :update, :destroy]
 
 	def index
 		@ytlinks = @song.ytlinks
 	end
 
 	def show 
-		@ytlink = @song.ytlinks.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def update
+		if @ytlink.update(ytlink_params)
+			redirect_to song_ytlinks_path(@song)
+		else
+			render :action => :edit
+		end
 	end
 
 	private
 
 	def set_song
 		@song = Song.find(params[:song_id])
+	end
+
+	def set_ytlink
+		@ytlink = @song.ytlinks.find(params[:id])
+	end
+
+	def ytlink_params
+		params.require(:ytlink).permit(:name,:link)
 	end
 
 end
