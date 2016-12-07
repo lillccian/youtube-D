371450b942ff052c5672827a3d5e1650a950bab0
lillccian <lillccian@gmail.com>
2016-12-07 18:27:15 +0800

    fix ui

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,11 @@
 class Song < ApplicationRecord
 	belongs_to :user
-	has_many :ytlinks
-	has_many :tags 
-  has_many :comments
-  has_many :keeps
+	has_many :ytlinks,:dependent=>:destroy 
+	has_many :tags ,:dependent=>:destroy 
+  has_many :comments ,:dependent=>:destroy 
+  has_many :keeps ,:dependent=>:destroy 
   has_many :keepedusers , :through => :keeps ,:dependent=>:destroy ,:source=> "user"
-  has_many :likes
+  has_many :likes ,:dependent=>:destroy 
   has_many :likedusers , :through => :likes ,:dependent=>:destroy ,:source=> "user"
 	def view!
     if self.view_count.nil?
