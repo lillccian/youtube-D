184378e90a320b6b61ebefb9c39a2460e24ba3d3
lillccian <lillccian@gmail.com>
2016-12-11 17:13:14 +0800

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
+	before_action :set_user, :only=>[:show, :edit, :update]
 	def show 
+	end
+	def edit 
+	end 
+	def update
+		if @user.update(params_user)
+			redirect_to songs_path
+		else
+			render :action=>:edit
+		end
+	end
+
+	private
+	def set_user
 		@user = User.find(params[:id])
 	end
+	def params_user
+		params.require(:user).permit(:nickname,:profile,:name)
+	end
+
 end
